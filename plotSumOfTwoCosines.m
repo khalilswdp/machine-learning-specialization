520a454fd51350afc993dec9471409dc677223f9
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-06-11 17:41:14 +0100

    first commit

---
+++ plotSumOfTwoCosines.m
@@ -0,0 +1,12 @@
+t=linspace(0,pi/2,1000);
+f1=12;
+f2=10;
+y=cos(2*pi*f1*t)+cos(2*pi*f2*t);
+envolope = [2*cos(pi*(f1-f2)*t); 2*cos(pi*(f1-f2)*t-pi)];
+
+figure
+hold on
+plot(t,y)
+
+plot(t,envolope)
+axis tight
